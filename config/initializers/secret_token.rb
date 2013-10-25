5281556c9dfd590014bd7957561bfcdf5fa496c9
Jiren Patel <jiren@joshsoftware.com>
2013-10-25 09:28:16 +0530

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FbScore::Application.config.secret_key_base = '058e78bb94fd0424551fbc9594cd81d50dea41ad330f33fcb1ecb923c3079322d46413e89d27a00526772c2763fb52373a2180a025ecfbe7601adc01e8eb6ebc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FbScore::Application.config.session_store :cookie_store, key: '_fb_score_session'
